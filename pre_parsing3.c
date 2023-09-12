913bf3e4a2e95eaa1b9f797a0b1ddf0cd51539da
Lucas Deville <ldeville@c1r3p4.42perpignan.fr>
2023-09-12 19:06:17 +0200

    replace home done

--- pre_parsing3.c
+++ pre_parsing3.c
@@ -6,43 +6,43 @@
 /*   By: ldeville <ldeville@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/12 17:07:27 by ldeville          #+#    #+#             */
-/*   Updated: 2023/09/12 18:24:45 by ldeville         ###   ########.fr       */
+/*   Updated: 2023/09/12 18:58:42 by ldeville         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-/*char	*replace_by(char *old, char *env, int here)
+char	*replace_by(char *old, char *env, int here)
 {
 	char	*str;
 	int		i;
 	int		y;
 
-	i = 0;
+	i = -1;
 	y = 0;
 	str = ft_calloc(ft_strlen(old) + ft_strlen(env), sizeof(char));
 	ft_printf("New len %i\n", ft_strlen(old) + ft_strlen(env));
-	while (i < here)
-		str[i] = old[i++];
-	
-	while (env[here])
-		str[i++] = env[here];
-	
-
-
-
+	while (++i < here)
+		str[i] = old[i];
+	while (env[y])
+		str[i++] = env[y++];
+	here++;
+	while (old[here])
+		str[i++] = old[here++];
+	free(old);
 	return (str);
 }
 
 void	find_home(t_mini *mini, t_lists *tmp)
 {
 	int	i;
+	(void) mini;
 
 	i = 0;
 	while (tmp->arg[i])
 	{
 		if (tmp->arg[i] == 126)
-			replace_home(tmp->arg, get_)
+			tmp->arg = replace_by(tmp->arg, (get_env(mini, "HOME") + 5), i);
 		if (tmp->arg[i] == '"' || tmp->arg[i] == '\'')
 		{
 			while (tmp->arg[i] && tmp->arg[i] != '"' && tmp->arg[i] != '\'')
@@ -50,7 +50,7 @@ void	find_home(t_mini *mini, t_lists *tmp)
 		}
 		i++;
 	}
-}*/
+}
 
 void	ft_replace(t_mini *mini)
 {
@@ -58,9 +58,10 @@ void	ft_replace(t_mini *mini)
 	
 	tmp = mini->args;
 
+	//printf("%s\n", (get_env(mini, "PWD") + 4));
 	while (tmp)
 	{
-		//find_home(mini, tmp);
+		find_home(mini, tmp);
 		//find_wildcard(mini, tmp);
 		//find_string_(); //"" et ''
 		tmp = tmp->next;
