21d74c1452be4afe4b141f02ae39edbcca3207a8
Lucas Deville <ldeville@c1r3p4.42perpignan.fr>
2023-09-12 15:38:22 +0200

    deleting space complete

--- pre_parsing2.c
+++ pre_parsing2.c
@@ -6,35 +6,38 @@
 /*   By: ldeville <ldeville@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/07 11:29:46 by ldeville          #+#    #+#             */
-/*   Updated: 2023/09/07 16:15:28 by ldeville         ###   ########.fr       */
+/*   Updated: 2023/09/12 15:37:51 by ldeville         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int	get_len_space(char *str)
+int	get_len_space(char *str, int num_arg)
 {
 	int	i;
 	int	len;
 
 	i = 0;
-	len = 0;
+	len = 1;
 	while (str[i])
 	{
 		while (str[i] == ' ' || (str[i] >= 9 && str[i] <= 13))
 			i++;
 		while (str[i] && str[i] > 32 && str[i] < 127)
 		{
-			if(str[i] == '"' || str[i] == '\'')
+			len++;
+			i++;
+			if (str[i - 1] == '"' || str[i - 1] == '\'')
 			{
 				while (str[++i] && (str[i] != '"' || str[i] != '\''))
 					len++;
+				len++;
 			}
-			i++;
-			len++;
 		}
+		if (str[i] && num_arg-- > 1)
+			len++;
 	}
-	ft_printf("len = %i", len);
+	ft_printf("len = %i\n", len);
 	return (len);
 }
 
@@ -46,25 +49,21 @@ char	*delete_space(char *old_str, int num_arg)
 
 	i = 0;
 	y = 0;
-	str = ft_calloc((get_len_space(old_str) + num_arg), sizeof(char));
+	str = ft_calloc((get_len_space(old_str, num_arg) + num_arg), sizeof(char));
 	while (old_str[i])
 	{
-		ft_printf("i = %i\n", i);
 		while (old_str[i] == ' ' || (old_str[i] >= 9 && old_str[i] <= 13))
 			i++;
-		ft_printf("2i = %i\n", i);
 		while (old_str[i] && old_str[i] > 32 && old_str[i] < 127)
 		{
-			ft_printf("3i = %i\n", i);
 			str[y++] = old_str[i++];
-			if(old_str[i - 1] == '"' || old_str[i - 1] == '\'')
+			if (old_str[i - 1] == '"' || old_str[i - 1] == '\'')
 			{
-				while (old_str[++i] && (old_str[i - 1] != '"' || old_str[i - 1] != '\''))
-					str[y++] = old_str[i];
-				ft_printf("4i = %i\n", i);
+				while (old_str[i] && (old_str[i] != '"' || old_str[i] != '\''))
+					str[y++] = old_str[i++];
+				str[y++] = old_str[i++];
 			}
 		}
-		ft_printf("5i = %i\n", i);
 		if (old_str[i] && num_arg-- > 1)
 			str[y++] = ' ';
 	}
@@ -76,7 +75,7 @@ void	ft_delete_space(t_mini *mini)
 	t_lists	*tmp;
 
 	tmp = mini->args;
-	while(tmp)
+	while (tmp)
 	{
 		tmp->arg = delete_space(tmp->arg, tmp->num_arg);
 		tmp = tmp->next;
@@ -98,7 +97,7 @@ int	get_num_arg(char *str)
 			count++;
 		while (str[i] && str[i] > 32 && str[i] < 127)
 		{
-			if(str[i] == '"' || str[i] == '\'')
+			if (str[i] == '"' || str[i] == '\'')
 			{
 				while (str[++i] && (str[i] != '"' || str[i] != '\''))
 					i++;
@@ -110,9 +109,9 @@ int	get_num_arg(char *str)
 	return (count);
 }
 
-void ft_add_num_arg(t_mini *mini)
+void	ft_add_num_arg(t_mini *mini)
 {
-	t_lists *tmp;
+	t_lists	*tmp;
 
 	tmp = mini->args;
 	while (tmp)
