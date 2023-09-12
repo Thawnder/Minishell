6019f1b18f7967792dc54333d96aa6bff6e63c4d
Lucas Deville <ldeville@c1r3p4.42perpignan.fr>
2023-09-12 17:22:50 +0200

    bizarreeeeeeeeeeeeeeeeeeeeeeeeee3

--- pre_parsing3.c
+++ pre_parsing3.c
@@ -6,7 +6,7 @@
 /*   By: ldeville <ldeville@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/12 17:07:27 by ldeville          #+#    #+#             */
-/*   Updated: 2023/09/12 17:09:27 by ldeville         ###   ########.fr       */
+/*   Updated: 2023/09/12 17:22:31 by ldeville         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,4 +17,9 @@ void	ft_replace_home(t_mini *mini)
 	t_lists	*tmp;
 	
 	tmp = mini->args;
+
+	while (tmp)
+	{
+		tmp = tmp->next;
+	}
 }
