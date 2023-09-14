7814eee86b504e08427fa98c56a4e64e0257dd4e
Thawnder <realthawnder@outlook.fr>
2023-09-14 14:51:59 +0200

    echo fix

--- commands2.c
+++ commands2.c
@@ -6,7 +6,7 @@
 /*   By: bpleutin <bpleutin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/05 11:22:03 by bpleutin          #+#    #+#             */
-/*   Updated: 2023/09/14 11:33:29 by bpleutin         ###   ########.fr       */
+/*   Updated: 2023/09/14 14:04:14 by bpleutin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -69,8 +69,8 @@ char	*stock_echo(char *line)
 			flag = 1;
 			i += 3;
 		}
-		/*if (line[i] == '-')
-					i += check_flag(line);*/
+		if (line[i] == '-')
+			i += check_flag(line);
 		res[j++] = line[i++];
 	}
 	if (flag == 0)
@@ -94,14 +94,9 @@ void	ft_echo(t_mini *mini, char *line)
 	{
 		while (line[i])
 		{
-			if (line[i] == '-' && line[i + 1] == 'n' && line[i + 2] == ' ')
-			{
-				flag = 1;
-				i += 3;
-			}
-			/*if (line[i] == '-')
-					i += check_flag(line);*/
-			write(1, line++, 1);
+			if (line[i] == '-')
+				i += check_flag(line);
+			write(1, &line[i++], 1);
 		}
 		if (flag == 0)
 			write(1, "\n", 1);
