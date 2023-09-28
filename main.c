7145043eda1cd9a3fc5cb38fe4bbee466bf01d13
Thawnder <realthawnder@outlook.fr>
2023-09-28 11:34:58 +0200

    free

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: bpleutin <bpleutin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/09 14:42:03 by ldeville          #+#    #+#             */
-/*   Updated: 2023/09/28 11:16:11 by bpleutin         ###   ########.fr       */
+/*   Updated: 2023/09/28 11:20:02 by bpleutin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,8 +20,12 @@ void	free_all(t_mini *mini)
 		free(mini->line);
 	if (mini->path)
 		free(mini->path);
-	/*if (mini->args->arg)
-		free_lists(mini);*/
+	if (mini->oldpath)
+		free(mini->oldpath);
+	if (mini->env)
+		free_tabl(mini->env);
+	if (mini->export)
+		free_tabl(mini->export);
 	rl_clear_history();
 	free(mini);
 }
