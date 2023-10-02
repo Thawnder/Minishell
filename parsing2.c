0d2acbca013f819606aac776a0975cbb740b8164
Lucas Deville <ldeville@c2r7p1.42perpignan.fr>
2023-10-02 18:10:10 +0200

    Pipe is the new Emperor

--- parsing2.c
+++ parsing2.c
@@ -6,7 +6,7 @@
 /*   By: ldeville <ldeville@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/28 11:41:33 by ldeville          #+#    #+#             */
-/*   Updated: 2023/10/02 15:24:10 by ldeville         ###   ########.fr       */
+/*   Updated: 2023/10/02 18:09:34 by ldeville         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,29 +14,36 @@
 
 void	child(t_mini *mini, t_lists *tmp, int pos)
 {
+	ft_printf("POS %i\n", pos);
 	if (pipe(mini->old_fd) < 0)
 		return ;
 	if (pos == 0)
 	{
+		write(1, "1", 1);
 		dup2(mini->old_fd[1], 1);
+		write(2, "2", 1);
 		ft_fork(mini, tmp->arg);
+		write(2, "3", 1);
 		close(mini->old_fd[1]);
+		ft_printf("ergertgert\n");
 	}
 	else if (pos == 1)
 	{
-		dup2(mini->new_fd[0], 0);
+		dup2(mini->old_fd[0], 0);
 		dup2(mini->old_fd[1], 1);
 		ft_fork(mini, tmp->arg);
-		close(mini->new_fd[0]);
+		close(mini->old_fd[0]);
 		close(mini->old_fd[1]);
 	}
 	else
 	{
+		write(2, "7", 1);
 		dup2(mini->old_fd[0], 0);
 		ft_fork(mini, tmp->arg);
+		mini->old_fd[1] = mini->old_fd[0];
 	}
-	mini->new_fd[0] = mini->old_fd[0];
-	mini->new_fd[1] = mini->old_fd[1];
+	mini->old_fd[0] = mini->old_fd[1];
+	write(2, "5", 1);
 }
 
 t_lists	*ft_pipe(t_mini *mini, t_lists *tmp)
@@ -44,7 +51,8 @@ t_lists	*ft_pipe(t_mini *mini, t_lists *tmp)
 	while (tmp && (tmp->operator == OP_PIPE
 		|| (tmp->previous && tmp->previous->operator == OP_PIPE)))
 	{
-		if (tmp->previous && tmp->previous->operator != OP_PIPE)
+		write(2, "6", 1);
+		if (!tmp->previous || (tmp->previous && tmp->previous->operator != OP_PIPE))
 			child(mini, tmp, 0);
 		else if (tmp->next && tmp->next->operator == OP_PIPE)
 			child(mini, tmp, 1);
@@ -52,6 +60,9 @@ t_lists	*ft_pipe(t_mini *mini, t_lists *tmp)
 			child(mini, tmp, 2);
 		tmp = tmp->next;
 	}
+	close(mini->old_fd[0]);
+	write(2, "10", 2);
+	ft_printf("ergertgert\n");
 	return (tmp);
 }
 
