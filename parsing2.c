34b9b7590b6b43a9ebac3a9696facb8efc897bcc
Lucas Deville <ldeville@c2r7p1.42perpignan.fr>
2023-10-03 11:24:25 +0200

    Pipe gonna be explosion

--- parsing2.c
+++ parsing2.c
@@ -6,7 +6,7 @@
 /*   By: ldeville <ldeville@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/28 11:41:33 by ldeville          #+#    #+#             */
-/*   Updated: 2023/10/02 18:09:34 by ldeville         ###   ########.fr       */
+/*   Updated: 2023/10/03 11:20:49 by ldeville         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,21 +14,18 @@
 
 void	child(t_mini *mini, t_lists *tmp, int pos)
 {
-	ft_printf("POS %i\n", pos);
 	if (pipe(mini->old_fd) < 0)
 		return ;
 	if (pos == 0)
 	{
-		write(1, "1", 1);
+		fprintf(stderr, "0\n");
 		dup2(mini->old_fd[1], 1);
-		write(2, "2", 1);
 		ft_fork(mini, tmp->arg);
-		write(2, "3", 1);
 		close(mini->old_fd[1]);
-		ft_printf("ergertgert\n");
-	}
+	} 
 	else if (pos == 1)
 	{
+		fprintf(stderr, "1\n");
 		dup2(mini->new_fd[0], 0);
 		dup2(mini->old_fd[1], 1);
 		ft_fork(mini, tmp->arg);
@@ -37,25 +34,26 @@ void	child(t_mini *mini, t_lists *tmp, int pos)
 	}
 	else
 	{
-		write(2, "7", 1);
+		fprintf(stderr, "2\n");
 		dup2(mini->new_fd[0], 0);
+		dup2(STDOUT_FILENO, 1);
 		ft_fork(mini, tmp->arg);
+		dup2(STDIN_FILENO, 0);
+		close(mini->new_fd[0]);
 	}
-	fprintf(stderr, "\n%i et %i\n", mini->new_fd[0], mini->new_fd[1]);
-	fprintf(stderr, "\n%i et %i\n", mini->old_fd[0], mini->old_fd[1]);
+	//fprintf(stderr, "\n%i et %i\n", mini->new_fd[0], mini->new_fd[1]);
+	//fprintf(stderr, "\n%i et %i\n", mini->old_fd[0], mini->old_fd[1]);
 	mini->new_fd[0] = mini->old_fd[0];
 	mini->new_fd[1] = mini->old_fd[1];
-	write(2, "5", 1);
 }
 
 t_lists	*ft_pipe(t_mini *mini, t_lists *tmp)
 {
-	mini->new_fd[0] = 0;
-	mini->new_fd[1] = 0;
+	if (pipe(mini->new_fd) < 0)
+		return (NULL);
 	while (tmp && (tmp->operator == OP_PIPE
 		|| (tmp->previous && tmp->previous->operator == OP_PIPE)))
 	{
-		write(2, "6", 1);
 		if (!tmp->previous || (tmp->previous && tmp->previous->operator != OP_PIPE))
 			child(mini, tmp, 0);
 		else if (tmp->next && tmp->next->operator == OP_PIPE)
@@ -64,8 +62,7 @@ t_lists	*ft_pipe(t_mini *mini, t_lists *tmp)
 			child(mini, tmp, 2);
 		tmp = tmp->next;
 	}
-	write(2, "10", 2);
-	ft_printf("ergertgert\n");
+	fprintf(stderr, "50\n");
 	return (tmp);
 }
 
