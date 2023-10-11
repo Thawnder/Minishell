2f5422795ec15d3f870f2fccfb58f29dfead5764
Benjamin Pleutin <bpleutin@c2r8p2.42perpignan.fr>
2023-10-11 15:11:16 +0200

    remove comments

--- pre_parsing2.c
+++ pre_parsing2.c
@@ -6,7 +6,7 @@
 /*   By: user <user@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/07 11:29:46 by ldeville          #+#    #+#             */
-/*   Updated: 2023/10/11 14:07:44 by user             ###   ########.fr       */
+/*   Updated: 2023/10/11 15:00:24 by bpleutin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,18 +29,11 @@ int	get_len_space(char *str)
 		{
 			len++;
 			if (is_quoted(str, i))
-			{
 				len += is_quoted(str, i);
+			if (is_quoted(str, i))
 				i += is_quoted(str, i);
-			}
 			else
 				i++;
-			/*if (str[++i - 1] == '"' || str[i - 1] == '\'')
-			{
-				while (str[i++] && str[i - 1] != '"' && str[i - 1] != '\'')
-					len++;
-				len++;
-			}*/
 		}
 		while (str[i] && (str[i] <= 32 || str[i] >= 127))
 			i++;
@@ -65,12 +58,6 @@ char	*delete_space(char *old_str, char *str)
 		{
 			str[y++] = old_str[i++];
 			ft_need_space(str, old_str, &i, &y);
-			/*if (old_str[i - 1] == '"' || old_str[i - 1] == '\'')
-			{
-				while (old_str[i] && old_str[i] != '"' && old_str[i] != '\'')
-					str[y++] = old_str[i++];
-				str[y++] = old_str[i++];
-			}*/
 		}
 		while (old_str[i] == ' ' || (old_str[i] >= 9 && old_str[i] <= 13))
 			i++;
@@ -110,12 +97,6 @@ int	get_num_arg(char *str)
 			i++;
 			if (is_quoted(str, i))
 				i += is_quoted(str, i);
-			/*if (str[i - 1] == '"' || str[i - 1] == '\'')
-			{
-				while (str[i] && str[i] != '"' && str[i] != '\'')
-					i++;
-				i++;
-			}*/
 		}
 	}
 	return (count);
